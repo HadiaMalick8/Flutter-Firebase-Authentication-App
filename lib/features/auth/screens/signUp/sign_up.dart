8e67e39bc180e950e025b791fa292007f26c58ed
Hadia Malick <143009761+HadiaMalick8@users.noreply.github.com>
2024-01-02 15:08:42 -0800

    Update sign_up.dart

--- lib/features/auth/screens/signUp/sign_up.dart
+++ lib/features/auth/screens/signUp/sign_up.dart
@@ -4,14 +4,9 @@ import '../../../../common_widgets/widgets.dart';
 import '../../../../constants/text_strings.dart';
 import 'widgets/signup_body.dart';
 
-class SignUp extends StatefulWidget {
+class SignUp extends StatelessWidget {
   const SignUp({super.key});
 
-  @override
-  State<SignUp> createState() => _SignUpState();
-}
-
-class _SignUpState extends State<SignUp> {
   @override
   Widget build(BuildContext context) {
     return const Background(headerText: signUp, body: SignUpBody(),);
